dfe104ccf63ad033f63153f15ea51dc2114be145
Cobbleopolis <cobbleopolis@gmail.com>
2019-03-06 20:55:01 -0500

    Changed back to the original problem

--- src/main/scala/com/cobble/ai/eightPuzzle/EightPuzzleProblem.scala
+++ src/main/scala/com/cobble/ai/eightPuzzle/EightPuzzleProblem.scala
@@ -4,31 +4,31 @@ import com.cobble.ai.core.Problem
 
 class EightPuzzleProblem extends Problem[EightPuzzleState, EightPuzzleNode] {
 
-    //    val INITIAL_STATE: EightPuzzleState = EightPuzzleState(Array(
-    //        2, 8, 3,
-    //        1, 6, 4,
-    //        7, 0, 5
-    //    ))
-    //
-    //    val GOAL_STATE: EightPuzzleState = EightPuzzleState(Array(
-    //        1, 2, 3,
-    //        8, 0, 4,
-    //        7, 6, 5
-    //    ))
+        val INITIAL_STATE: EightPuzzleState = EightPuzzleState(Array(
+            2, 8, 3,
+            1, 6, 4,
+            7, 0, 5
+        ))
 
-    val INITIAL_STATE: EightPuzzleState = EightPuzzleState(Array(
-        1, 2, 3, 4,
-        5, 6, 0, 8,
-        9, 10, 7, 11,
-        13, 14, 15, 12
-    ))
+        val GOAL_STATE: EightPuzzleState = EightPuzzleState(Array(
+            1, 2, 3,
+            8, 0, 4,
+            7, 6, 5
+        ))
 
-    val GOAL_STATE: EightPuzzleState = EightPuzzleState(Array(
-        1, 2, 3, 4,
-        5, 6, 7, 8,
-        9, 10, 11, 12,
-        13, 14, 15, 0
-    ))
+//    val INITIAL_STATE: EightPuzzleState = EightPuzzleState(Array(
+//        1, 2, 3, 4,
+//        5, 6, 0, 8,
+//        9, 10, 7, 11,
+//        13, 14, 15, 12
+//    ))
+//
+//    val GOAL_STATE: EightPuzzleState = EightPuzzleState(Array(
+//        1, 2, 3, 4,
+//        5, 6, 7, 8,
+//        9, 10, 11, 12,
+//        13, 14, 15, 0
+//    ))
 
     if (!INITIAL_STATE.isValid)
         throw new ExceptionInInitializerError("Initial State not valid!")
